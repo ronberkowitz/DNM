6039b725965b6c179cd797890304c98f33f9d603
Ron Berkowitz <ron@storemaven.com>
2019-01-25 14:00:38 +0200

    The Beginning

---
+++ routes.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def load_index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)
